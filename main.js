186e94389a3158d371f4ddbfc48f0d2910e1aa77
Bea <beattrizcruzzz@gmail.com>
2025-10-16 10:18:04 -0300

    aula 3

---
+++ main.js
@@ -0,0 +1,24 @@
+function tocaSom(idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+for (let i = 0; i < listaDeTeclas.length; i++) {
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1].replace('tecla_', '');
+    const idAudio = `#som_tecla_${instrumento}`;
+
+    tecla.onclick = function() {
+        tocaSom(idAudio);
+    };
+}
+
+tecla.onkeydown = function (event) {
+    if (event.code ==='Space' || event.code ==='Enter'){
+    tecla.classList.add('ativa');
+    }
+}
+
+tecla.onkeyup = function (event) {
+    tecla.classList.remove('ativa');
+}
